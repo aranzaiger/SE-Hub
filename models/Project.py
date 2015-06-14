7239d27da2a13e34b064076c7df2748ec85f1004
aranzaiger <aranzaiger@gmail.com>
2015-06-15 01:35:18 +0300

    Projects - added master_id and changed some properties

--- models/Project.py
+++ models/Project.py
@@ -5,13 +5,15 @@ from google.appengine.ext import db
 
 class Project(db.Model):
     projectName = db.StringProperty(required=True)
-    grade = db.IntegerProperty(required=True)
-    logo_url = db.StringProperty(required=True)
+    masterId = db.IntegerProperty(required=True)
+    grade = db.IntegerProperty(required=True, default=0)
+    logo_url = db.StringProperty(required=False)
     gitRepository = db.StringProperty(required=True)
     membersId = db.StringListProperty(required=True)
 
     def to_JSON(self):
         data = {'projectName' : self.projectName,
+                'masterId' : self.masterId,
                 'grade' : self.grade,
                 'logo_url' : self.logo_url,
                 'gitRepository' : self.gitRepository,
