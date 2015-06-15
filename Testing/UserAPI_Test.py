2dc319af48106e2a8b22fd06944a3c0e9c064ad8
Sagi Dayan <sagidayan@gmail.com>
2015-06-15 16:28:54 +0300

    QA: Update UserAPI_Test.py to new API scheme

--- Testing/UserAPI_Test.py
+++ Testing/UserAPI_Test.py
@@ -17,18 +17,27 @@ class UserTestPlan(unittest.TestCase):
             print 'Initialized'
 
     def test_getUserByToken_invalid(self):
-        r = requests.get(self.__class__.url_+'api/getUserByToken/invalidtoken')
+        r = requests.get(self.__class__.url_+'api/users/getUserByToken/invalidtoken')
         self.assertEquals(r.status_code, 403)
 
     def test_getUserByToken_valid(self):
-        r = requests.get(self.__class__.url_+'api/getUserByToken/'+__CONFIG__['TOKENS']['STUDENT'])
+        r = requests.get(self.__class__.url_+'api/users/getUserByToken/'+__CONFIG__['TOKENS']['STUDENT'])
         self.assertEquals(r.status_code, 200)
         self.assertEquals(r.json()['username'], 'qa_student')
 
     def test_getUserByToken_empty(self):
-         r = requests.get(self.__class__.url_+'api/getUserByToken/')
+         r = requests.get(self.__class__.url_+'api/users/getUserByToken/')
          self.assertEquals(r.status_code, 400)
 
+    def test_isStudent_Student(self):
+        r = requests.get(self.__class__.url_+'api/users/getUserByToken/'+__CONFIG__['TOKENS']['STUDENT'])
+        self.assertEquals(r.status_code, 200)
+        self.assertFalse(r.json()['isLecturer'])
+
+    def test_isLecturer_Lecturer(self):
+        r = requests.get(self.__class__.url_+'api/users/getUserByToken/'+__CONFIG__['TOKENS']['LECTURER'])
+        self.assertEquals(r.status_code, 200)
+        self.assertTrue(r.json()['isLecturer'])
 
 
 if __name__ == '__main__':
