ab4cdab12da2ccb721b2fd938567e317419d7398
aranzaiger <aranzaiger@gmail.com>
2015-06-15 18:34:02 +0300

    removed project list from course and added course to project

--- models/Course.py
+++ models/Course.py
@@ -7,7 +7,7 @@ class Course(db.Model):
     courseName = db.StringProperty(required=True)
     campusName = db.StringProperty(required=True)
     master_id = db.IntegerProperty(required=True)
-    projects = db.StringListProperty(required=True,default=[])
+    # projects = db.StringListProperty(required=True,default=[])
     startDate = db.DateProperty(required=True)
     endDate = db.DateProperty(required=True)
 
@@ -15,7 +15,7 @@ class Course(db.Model):
         data = {'courseName' : self.courseName,
                 'campusName' : self.campusName,
                 'master_id' : self.master_id,
-                'projects' : self.projects,
+                # 'projects' : self.projects,
                 'startDate' : {
                     'year': self.startDate.year,
                     'month': self.startDate.month,
