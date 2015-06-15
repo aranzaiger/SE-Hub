379ede2617e561ffe03b4ecc6c5d783f834fe7be
aranzaiger <aranzaiger@gmail.com>
2015-06-15 16:32:35 +0300

    models - added master_id

--- models/Project.py
+++ models/Project.py
@@ -5,7 +5,7 @@ from google.appengine.ext import db
 
 class Project(db.Model):
     projectName = db.StringProperty(required=True)
-    masterId = db.IntegerProperty(required=True)
+    master_id = db.IntegerProperty(required=True)
     grade = db.IntegerProperty(required=True, default=0)
     logo_url = db.StringProperty(required=False)
     gitRepository = db.StringProperty(required=True)
@@ -13,7 +13,7 @@ class Project(db.Model):
 
     def to_JSON(self):
         data = {'projectName' : self.projectName,
-                'masterId' : self.masterId,
+                'master_id' : self.master_id,
                 'grade' : self.grade,
                 'logo_url' : self.logo_url,
                 'gitRepository' : self.gitRepository,
