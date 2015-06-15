06a883e103cf61f9371849b538cf20345734b7f3
Matan Bar Yosef <baryosef.matan@gmail.com>
2015-06-15 19:12:54 +0300

    documentation added alpha test for userPrivilege

--- templates/js/controllers/registerController.js
+++ templates/js/controllers/registerController.js
@@ -57,7 +57,7 @@ angular.module('SeHub')
 			  	$mdDialog.show($mdDialog.alert().title('E-mail Verification').content('A verification e-mail has been sent to your email address.')
 		        .ariaLabel('Email verification alert dialog').ok('Got it!').targetEvent(ev)); // Pop-up alert for e-mail verification
 		        $cookieStore.remove("com.sehub.www"); // Removing the cookies
-		        $window.location.href = 'http://se-hub.appspot.com';
+		        $window.location.href = 'http://se-hub.appspot.com'; // Reference to 'welcome' page
 			}).error(function()
 			{
 				$mdDialog.show($mdDialog.alert().title('Error - E-mail Verification').content('An error has occured in your e-mail address.')
@@ -65,6 +65,17 @@ angular.module('SeHub')
 			});
 		};
 	};
+
+	$scope.lecturerPrivilege = function(data)
+	{
+		// console.log("Now " + data);
+		var isLecturer;
+		if(!data) // if i am a lecturer (when pressing -> getting last data value before pressing) = "!data" => I Am Lecturer
+		{
+			isLecturer = true;
+			console.log("im lecturer " + isLecturer);
+		}
+	}
 
 
 	// TODO FOR LATER - toast
