51d11cac938fdb3ba010f16a24a392bbf6fc2970
Sagi Dayan <sagidayan@gmail.com>
2015-06-17 22:46:26 +0300

    More Setings UI Tweaks

--- templates/js/controllers/settingsController.js
+++ templates/js/controllers/settingsController.js
@@ -1,57 +1,78 @@
 angular.module('SeHub')
-	.controller('settingsController', ['$scope', '$rootScope', 'dataService','apiService', '$cookies', '$location', 
-		function($scope, $rootScope, dataService ,apiService, $cookies, $location) {
+	.controller('settingsController', ['$scope', '$rootScope', 'dataService', 'apiService', '$cookies', '$location',
+		function($scope, $rootScope, dataService, apiService, $cookies, $location) {
 
-		var token = $cookies['com.sehub.www'];
+			var token = $cookies['com.sehub.www'];
 
-		$scope.loadingData = true;
-		$scope.isInRegisterMode = false;
+			$scope.loadingData = true;
+			$scope.isInRegisterMode = false;
 
-		$scope.title = "Settings"
+			$scope.title = "Settings"
 
-		apiService.getUserByToken(token).success(function(data) {
-			if (data.message == 'No User Found') {
-				console.error("No User Found!");
-			}
-			$scope.loadingData = false;
-			$scope.user = data;
-			console.log(data);
-			if ($scope.user.isFirstLogin) {
-				$scope.menuObj = {};
-				$scope.isInRegisterMode = true;
+			apiService.getUserByToken(token).success(function(data) {
+				if (data.message == 'No User Found') {
+					console.error("No User Found!");
+				}
 				$scope.loadingData = false;
-				$location.path('/register')
+				$scope.user = data;
+				console.log(data);
+				if ($scope.user.isFirstLogin) {
+					$scope.menuObj = {};
+					$scope.isInRegisterMode = true;
+					$scope.loadingData = false;
+					$location.path('/register')
+				}
+
+			});
+
+			$scope.isEditMode = false;
+			$scope.profileMode = "Edit Profile";
+			$scope.profileModeIcon = "fa fa-pencil";
+
+			$scope.changeProfileMode = function() {
+				$scope.isEditMode = !$scope.isEditMode;
+				if ($scope.isEditMode) {
+					$scope.profileMode = "Save Profile";
+					$scope.profileModeIcon = "fa fa-floppy-o";
+				} else {
+					dataService.userBrodcast($scope.user);
+					$scope.profileMode = "Edit Profile";
+					$scope.profileModeIcon = "fa fa-pencil";
+				}
 			}
 
-		});
+			/**
+			 * DEBUG DATA
+			 */
+			$scope.courses = [{
+				"courseName": "Advance Math",
+				"campusName": "JCE",
+				"startDate": {
+					"year": 2015,
+					"month": 4,
+					"day": 3
+				},
+				"endDate": {
+					"year": 2016,
+					"month": 5,
+					"day": 14
+				},
+				"taskFlag": false,
+				"campus_avatar": "https://yt3.ggpht.com/--ZkWxybWGOM/AAAAAAAAAAI/AAAAAAAAAAA/_nAICC_kzzI/s88-c-k-no/photo.jpg"
+			}];
 
-		$scope.isEditMode = false;
-		$scope.profileMode = "Edit Profile";
-		$scope.profileModeIcon = "fa fa-pencil";
+			$scope.campuses = [{
+				'title': 'JCE',
+				'email_ending': '@post.jce.ac.il',
+				'master_user_id': 123453433341,
+				'avatar_url': 'https://yt3.ggpht.com/--ZkWxybWGOM/AAAAAAAAAAI/AAAAAAAAAAA/_nAICC_kzzI/s88-c-k-no/photo.jpg'
+			}, {
+				'title': 'Stanford',
+				'email_ending': '@post.jce.ac.il',
+				'master_user_id': 123453433341,
+				'avatar_url': 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcR9M4uQgaJP1zyiCGw-dK31hU8buWqeuOi9vTXBd4Y8hQcFTZqA'
+			}];
 
-		$scope.changeProfileMode = function() {
-			$scope.isEditMode = !$scope.isEditMode;
-			if ($scope.isEditMode) {
-				$scope.profileMode = "Save Profile";
-				$scope.profileModeIcon = "fa fa-floppy-o";
-			} else {
-				dataService.userBrodcast($scope.user);
-				$scope.profileMode = "Edit Profile";
-				$scope.profileModeIcon = "fa fa-pencil";
-			}
+
 		}
-
-		// {
-		// name: ";"
-		// isLecturer: false
-		// email: "sagidayan@gmail.com"
-		// username: "sagidayan"
-		// seToken: "76cd4178-94dd-4cb4-b464-111d2239e567"
-		// isFirstLogin: true
-		// campuses_id_list: [0]
-		// classes_id_list: [0]
-		// avatar_url: "https://avatars.githubusercontent.com/u/2984053?v=3"
-		// }
-
-
-	}]);
+	]);
