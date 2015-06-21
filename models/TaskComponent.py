cd0fb3cf4fc76835077f30fc607ee3027a9f7ca6
aranzaiger <aranzaiger@gmail.com>
2015-06-21 23:53:39 +0300

    Task Rework - chaged parameters                           added components module and creation in create task                           added getTaskComponents function

--- models/TaskComponent.py
+++ models/TaskComponent.py
@@ -6,27 +6,19 @@ from google.appengine.ext import db
 
 
 class TaskComponent(db.Model):
-    taskId = db.StringProperty(required=True)
-    courseName = db.StringProperty(required=True)
-    description = db.StringProperty(required=True,default=" ")
-    dueDate = db.DateProperty(required=True)
-    #isProject = db.BooleanProperty(required=False)
-    isClose = db.BooleanProperty(required=True, default=False)
-    isDone = db.BooleanProperty(required=True, default=False)
-    taskGrade = db.IntegerProperty(required=True, default=0)
+    taskId = db.IntegerProperty(required=True)
+    userId = db.IntegerProperty(required=True, default = -1)
+    type = db.StringProperty(required=True,default=" ")
+    label = db.StringProperty(required=True,default=" ")
+    isMandatory = db.BooleanProperty(required=True, default=True)
+    order = db.IntegerProperty(required=True)
 
     def to_JSON(self):
-        data = {'title' : self.title,
-                'courseName' : self.courseName,
-                'description' : self.description,
-                 'dueDate' : {
-                    'year': self.dueDate.year,
-                    'month': self.dueDate.month,
-                    'day': self.dueDate.day
-                },
-                #'isProject' : self.isProject,
-                'isClose' : self.isClose,
-                'isDone' : self.isDone,
-                'taskGrade' : self.taskGrade,
+        data = {'taskId' : self.taskId,
+                'userId' : self.userId,
+                'type' : self.type,
+                'label' : self.label,
+                'isMandatory' : self.isMandatory,
+                'order' : self.order
                 }
         return json.dumps(data)
