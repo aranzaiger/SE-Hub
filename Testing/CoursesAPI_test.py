0a02aaf69211e3da002ddfddfa7ee3c263a189b3
Sagi Dayan <sagidayan@gmail.com>
2015-06-23 19:55:58 +0300

    Reverting Arans commit on testing

--- Testing/CoursesAPI_test.py
+++ Testing/CoursesAPI_test.py
@@ -3,7 +3,8 @@ import unittest
 import requests
 import json
 from Testing.config import __CONFIG__
-class CoursesTestPlan(unittest.TestCase):
+
+class UserTestPlan(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         debug = __CONFIG__['DEBUG']
@@ -16,75 +17,80 @@ class CoursesTestPlan(unittest.TestCase):
         if 200 <= request.status_code <= 299:
             print 'Initialized'
 
-    def test_coursesCreate_Lecturer(self):
-        headers = {'content-type': 'application/json'}
-        url = 'http://se-hub.appspot.com/api/courses/create/_QA_TOKEN_TEST_LECTURER'
-        params = {'seToken': 'seToken' }
+    def test_courseCreate_lecturer(self):
+        #url = "http://localhost:8080/api/courses/create/_QA_TOKEN_TEST_LECTURER"
+        url=self.__class__.url_+'api/courses/create/'+__CONFIG__['TOKENS']['LECTURER']
         data = {
-            'courseName': 'QA COURSE',
+            'courseName': 'matan',
             'campusName': 'https://yt3.ggpht.com/--ZkWxybWGOM/AAAAAAAAAAI/AAAAAAAAAAA/_nAICC_kzzI/s88-c-k-no/photo.jpg',
             'projects': '@gay.lord.ultima.multi.omega',
             'startDate': {'year': 2015, 'month' : 4, 'day' : 3},
             'endDate': {'year': 2016,'month' : 6,'day' : 6}
         }
+        headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+        r = requests.post(url, data=json.dumps(data), headers=headers)
+        self.assertEquals(r.status_code, 201)
 
-        #r = requests.post(self.__class__.url_+'api/courses/create/'+__CONFIG__['TOKENS']['STUDENT'],data=payload)
-        r = requests.post(url, params=params, data=json.dumps(data), headers=headers)
-        '''
-        payload = {
-            'courseName': 'QA COURSE',
-            'campusName': 'https://yt3.ggpht.com/--ZkWxybWGOM/AAAAAAAAAAI/AAAAAAAAAAA/_nAICC_kzzI/s88-c-k-no/photo.jpg',
-            'projects': '@gay.lord.ultima.multi.omega',
-            'startDate': {'year': 2015, 'month' : 4, 'day' : 3},
-            'endDate': {'year': 2016,'month' : 6,'day' : 6}
-        }
-        '''
-        # r = requests.post(self.__class__.url_+'api/courses/create/'+__CONFIG__['TOKENS']['LECTURER'],data=payload)
-        #r = requests.post('http://localhost:8080/api/courses/create/_QA_TOKEN_TEST_LECTURER',data=payload)
-        #self.assertEquals(r.status_code, 201)
-
-    def test_coursesCreate_InvalidToken(self):
-        headers = {'content-type': 'application/json'}
-        url = self.__class__.url_+'api/courses/create/invalidToken'
+    def test_courseCreate_lecturerExsistingCourse(self):
+        #url = "http://localhost:8080/api/courses/create/_QA_TOKEN_TEST_LECTURER"
+        url=self.__class__.url_+'api/courses/create/'+__CONFIG__['TOKENS']['LECTURER']
         data = {
-            'courseName': 'QA COURSE',
+            'courseName': 'matan',
             'campusName': 'https://yt3.ggpht.com/--ZkWxybWGOM/AAAAAAAAAAI/AAAAAAAAAAA/_nAICC_kzzI/s88-c-k-no/photo.jpg',
             'projects': '@gay.lord.ultima.multi.omega',
             'startDate': {'year': 2015, 'month' : 4, 'day' : 3},
             'endDate': {'year': 2016,'month' : 6,'day' : 6}
         }
-        params = {'seToken': 'seToken' }
-        r = requests.post(url, params=params, data=json.dumps(data), headers=headers)
+        headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+        r = requests.post(url, data=json.dumps(data), headers=headers)
         self.assertEquals(r.status_code, 403)
-        '''
-        payload = {'courseName': 'Advance Math', 'campusName': 'JCE', 'startDate':'2015-14-3','endDate': '2015-29-6','taskFlag': 'False'}
-        payload = {
-            'courseName': 'QA COURSE',
-            'campusName': 'https://yt3.ggpht.com/--ZkWxybWGOM/AAAAAAAAAAI/AAAAAAAAAAA/_nAICC_kzzI/s88-c-k-no/photo.jpg',
-            'projects': '@gay.lord.ultima.multi.omega',
-            'startDate': {'year': 2015, 'month' : 4, 'day' : 3},
-            'endDate': {'year': 2016,'month' : 6,'day' : 6}
-        }
-        r = requests.post(self.__class__.url_+'api/courses/create/invalidToken',data=payload)
-        '''
-    def test_coursesCreate_Student(self):
-        headers = {'content-type': 'application/json'}
-        url = self.__class__.url_+'api/courses/create/_QA_TOKEN_TEST_STUDENT'
-        params = {'seToken': 'seToken' }
+
+    def test_courseCreate_student(self):
+        #url = "http://localhost:8080/api/courses/create/_QA_TOKEN_TEST_LECTURER"
+        url=self.__class__.url_+'api/courses/create/'+__CONFIG__['TOKENS']['STUDENT']
         data = {
-            'courseName': 'QA COURSE',
+            'courseName': 'matan',
             'campusName': 'https://yt3.ggpht.com/--ZkWxybWGOM/AAAAAAAAAAI/AAAAAAAAAAA/_nAICC_kzzI/s88-c-k-no/photo.jpg',
             'projects': '@gay.lord.ultima.multi.omega',
             'startDate': {'year': 2015, 'month' : 4, 'day' : 3},
             'endDate': {'year': 2016,'month' : 6,'day' : 6}
         }
+        headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+        r = requests.post(url, data=json.dumps(data), headers=headers)
+        self.assertEquals(r.status_code, 403)
 
-        #r = requests.post(self.__class__.url_+'api/courses/create/'+__CONFIG__['TOKENS']['STUDENT'],data=payload)
-        r = requests.post(url, params=params, data=json.dumps(data), headers=headers)
-        self.assertEquals(r.status_code, 403, 'message: ' + r.json()['message'])
+    def test_courseCreate_invalidToken(self):
+        #url = "http://localhost:8080/api/courses/create/_QA_TOKEN_TEST_LECTURER"
+        url=self.__class__.url_+'api/courses/create/invalidToken'
+        data = {
+            'courseName': 'matan',
+            'campusName': 'https://yt3.ggpht.com/--ZkWxybWGOM/AAAAAAAAAAI/AAAAAAAAAAA/_nAICC_kzzI/s88-c-k-no/photo.jpg',
+            'projects': '@gay.lord.ultima.multi.omega',
+            'startDate': {'year': 2015, 'month' : 4, 'day' : 3},
+            'endDate': {'year': 2016,'month' : 6,'day' : 6}
+        }
+        headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+        r = requests.post(url, data=json.dumps(data), headers=headers)
+        self.assertEquals(r.status_code, 403)
 
-    def test_getCourseByCampusName(self):
-        r = requests.get(self.__class__.url_+'api/courses/getCourseByCampusName/'+__CONFIG__['TOKENS']['CAMPUS_NAME'])
+    def test_courseCreate_hebrewToken(self):
+        #url = "http://localhost:8080/api/courses/create/_QA_TOKEN_TEST_LECTURER"
+        url=self.__class__.url_+'api/courses/create/????'
+        data = {
+            'courseName': 'matan',
+            'campusName': 'https://yt3.ggpht.com/--ZkWxybWGOM/AAAAAAAAAAI/AAAAAAAAAAA/_nAICC_kzzI/s88-c-k-no/photo.jpg',
+            'projects': '@gay.lord.ultima.multi.omega',
+            'startDate': {'year': 2015, 'month' : 4, 'day' : 3},
+            'endDate': {'year': 2016,'month' : 6,'day' : 6}
+        }
+        headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+        r = requests.post(url, data=json.dumps(data), headers=headers)
+        self.assertEquals(r.status_code, 404)
+
+    #/api/courses/getCourseByCampusName/<string:name>
+    def test_getCourseByCampusName_EXSISTING_CAMPUS(self):
+        url=self.__class__.url_+'api/courses/getCourseByCampusName/'+__CONFIG__['CAMPUS_NAME']['JCE']
+        r = requests.get(url)
         self.assertEquals(r.status_code, 200)
 
 if __name__ == '__main__':
