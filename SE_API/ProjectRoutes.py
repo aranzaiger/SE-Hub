af98c69723c9d56bfe2023a2c6faaf246619001c
aranzaiger <aranzaiger@gmail.com>
2015-06-24 20:09:13 +0300

    added getProjectsByUser and joinProject function

--- SE_API/ProjectRoutes.py
+++ SE_API/ProjectRoutes.py
@@ -58,7 +58,10 @@ def create_project(token):
     """
     if not request.data:
         return bad_request()
-    payload = json.loads(request.data)
+    try:
+        payload = json.loads(request.data)
+    except Exception as e:
+        return bad_request("invalid JSON format")
     #if not is_lecturer(token):  #todo: change to lecturer id
      #   return forbidden("Invalid token or not a lecturer!")
 
@@ -68,6 +71,7 @@ def create_project(token):
 
     #todo: check legality
 
+
     try:
         project = Project(projectName=payload['projectName'], courseName=payload['courseName'], master_id=user.key().id(), gitRepository=payload['gitRepository'], membersId=[token])
     except Exception as e:
@@ -75,6 +79,11 @@ def create_project(token):
         return bad_request()
 
     db.put(project)
+
+    #update user projects list
+    user.projects_id_list.append(str(project.key().id()))
+
+    db.put(user)
     db.save
     return Response(response=project.to_JSON(),
                                 status=200,
@@ -83,6 +92,50 @@ def create_project(token):
 #----------------------------------------------------------
 #                     PUT
 #----------------------------------------------------------
+
+@project_routes.route('/api/projects/joinProject/<string:token>/<string:projectId>', methods=["PUT"])
+@auto.doc()
+def joinProject(token, projectId):
+    """
+    <span class="card-title">This call will add the user (by token) to a specific project</span>
+    <br>
+    <b>Route Parameters</b><br>
+        - seToken: 'seToken'<br>
+        - projectId: 123456789
+    <br>
+    <br>
+    <b>Payload</b><br>
+     - None <br>
+    <br>
+    <b>Response</b>
+    <br>
+    202 - Accepted
+    <br>
+    400 - Bad Request
+    <br>
+    403 - Invalid token or not a lecturer
+    """
+
+    user = get_user_by_token(token)
+    if user is None:
+        return bad_request("Wrong user Token")
+
+    project = Project.get_by_id(int(projectId))
+    if project is None:
+        return bad_request("No such Project")
+
+    if user.key().id() in project.membersId:
+        return no_content("User is already member in Project")
+
+    project.membersId.append(str(user.key().id()))
+
+    db.put(project)
+    db.save
+
+    return Response(response=project.to_JSON(),
+                        status=202,
+                        mimetype="application/json")
+
 
 
 #----------------------------------------------------------
@@ -138,21 +191,73 @@ def getProjectsByCourseName(name):
 
 
 
+@project_routes.route('/api/projects/getProjectsByUser/<string:token>', methods=["GET"])
+@auto.doc()
+def getProjectsByUser(token):
+    """
+    <span class="card-title">>This Call will return an array of all projects in a given course</span>
+    <br>
+    <b>Route Parameters</b><br>
+        - token: 'SEToken'
+    <br>
+    <br>
+    <b>Payload</b><br>
+     - NONE
+    <br>
+    <br>
+    <b>Response</b>
+    <br>
+    200 - JSON Example:<br>
+    <code>
+        {<br>
+        'projectName': 'Advance Math',<br>
+        'courseName': 'JCE',<br>
+        'grade': 98,<br>
+        'logo_url': 'http://location.domain.com/image.jpg',<br>
+        'gitRepository': 'http://location.git.com/somthing',<br>
+        'membersId': ['bob', 'dylan', 'quentin', 'terentino'],<br>
+        'id' : 1234567890<br>
+        }
+    </code>
+    <br>
+    """
+
+    user = get_user_by_token(token)
+    if user is None:
+        return bad_request("Bad Token")
+
+
+    arr = []
+    for p in user['projects_id_list']:
+        project = Project.get_by_id(int(p))
+        arr.append(dict(json.loads(project.to_JSON())))
+
+    if len(arr) != 0:
+        return Response(response=json.dumps(arr),
+                        status=200,
+                        mimetype="application/json")
+    else:
+        return Response(response=[],
+                        status=200,
+                        mimetype="application/json")
+
+
+
 #----------------------------------------------------------
 #                     DELETE
 #----------------------------------------------------------
 
 
 
-@project_routes.route('/api/projects/deleteProject/<string:token>/<string:projectid>', methods=['DELETE'])
+@project_routes.route('/api/projects/deleteProject/<string:token>/<string:projectId>', methods=['DELETE'])
 @auto.doc()
-def deleteProject(token,projectid):
+def deleteProject(token,projectId):
     """
     <span class="card-title">This Call will delete a specific Project</span>
     <br>
     <b>Route Parameters</b><br>
         - seToken: 'seToken'
-        - courseid: 'projectid'
+        - projectId: 'projectid'
     <br>
     <br>
     <b>Payload</b><br>
@@ -178,7 +283,9 @@ def deleteProject(token,projectid):
     #     return forbidden("Invalid token or not a lecturer!")
 
     user = get_user_by_token(token)
-    p = Project.get_by_id(int(projectid))
+    if user is None:
+        return bad_request("Bad user Token")
+    p = Project.get_by_id(int(projectId))
 
     if p is None:
         return bad_request("no such Project")
