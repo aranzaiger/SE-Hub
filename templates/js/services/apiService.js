91ac400452b3da90c541c01a5345a5956f54a903
Matan Bar Yosef <baryosef.matan@gmail.com>
2015-06-26 16:58:55 +0300

    Merge branch 'master' of https://github.com/sagidayan/SE-Hub into UI
    # Conflicts:
    #	templates/js/services/apiService.js

--- templates/js/services/apiService.js
+++ templates/js/services/apiService.js
@@ -100,8 +100,6 @@ service.factory('apiService', ['$http', function($http) {
 			return $http(req);
 		},
 		getCampusesByUser: function(token){
-			var url =  (DEBUG ? "http://localhost:8080" : "http://se-hub.appspot.com") + "/api/courses/getCampusesByUser/" + token;
-		getCampusesByUser: function(token){
 			var url =  (DEBUG ? "http://localhost:8080" : "http://se-hub.appspot.com") + "/api/campuses/getCampusesByUser/" + token;
 			req = {
 				method : "GET",
