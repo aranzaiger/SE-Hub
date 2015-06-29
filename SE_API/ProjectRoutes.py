1c1a2f7b543e3da4905ce061043e23009d3ff747
aranzaiger <aranzaiger@gmail.com>
2015-06-29 21:31:47 +0300

    fix3

--- SE_API/ProjectRoutes.py
+++ SE_API/ProjectRoutes.py
@@ -86,8 +86,8 @@ def create_project(token):
         print e
         pass
 
-    project.info = json.dumps(get_github_data(project.gitRepository))
     db.put(project)
+    project.info = json.dumps(get_github_data(project.gitRepository,  project.key().id()))
 
     #update user projects list
     user.projects_id_list.append(str(project.key().id()))
@@ -395,7 +395,7 @@ def updateProjectsInfo():
     try:
         projects = Project.all()
         for project in projects.run():
-            project.info = json.dumps(get_github_data(project.gitRepository))
+            project.info = json.dumps(get_github_data(project.gitRepository, project.key().id()))
             db.put(project)
         db.save
         return accepted()
