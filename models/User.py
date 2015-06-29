57b08c2ff1559ea384ef19a5721c0eb2e57fe316
aranzaiger <aranzaiger@gmail.com>
2015-06-29 13:32:01 +0300

    removed campusName in user module

--- models/User.py
+++ models/User.py
@@ -12,7 +12,6 @@ class User(db.Model):
     seToken = db.StringProperty(required=True)
     avatar_url = db.StringProperty(required=True)
     isFirstLogin = db.BooleanProperty(default=True)
-    campusName = db.StringProperty(required=True, default=" ")
     campuses_id_list = db.StringListProperty(default=[])
     courses_id_list = db.StringListProperty(default=[])
     projects_id_list = db.StringListProperty(default=[])
@@ -24,7 +23,6 @@ class User(db.Model):
                 'isLecturer' : self.isLecturer,
                 'avatar_url' : self.avatar_url,
                 'isFirstLogin' : self.isFirstLogin,
-                'campusName': self.campusName,
                 'campuses_id_list': self.campuses_id_list,
                 'courses_id_list': self.courses_id_list,
                 'projects_id_list': self.projects_id_list,
