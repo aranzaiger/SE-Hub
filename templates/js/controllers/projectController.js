59ff8a176f322a232453382da21ce278d624f8a9
Matan Bar Yosef <baryosef.matan@gmail.com>
2015-06-30 15:56:53 +0300

    - Project layout - fixed
    - project tabs - commits, issues... (work in progress - see the alpha version)

--- templates/js/controllers/projectController.js
+++ templates/js/controllers/projectController.js
@@ -7,19 +7,28 @@ angular.module('SeHub')
 	$scope.isEditPressed = false;
 	$scope.user = $scope.$parent.user;
 	$scope.loadingData = true;
+	$scope.isMasterOrLecturer = false;
 
 	// $scope.thisProject = {};
 	// $scope.thisProject.courseName = $routeParams.className;
 
-	$scope.editProject = function()
+	$scope.editProject = function(ev)
 	{
 		$scope.isEditPressed = true;
 		console.log("EditPressed " + $scope.isEditPressed);
 	}
-	$scope.removeProject = function()
+
+	$scope.removeProject = function(ev)
 	{
 		console.log("Project has been removed!");
-	}
+		$mdDialog.show($mdDialog.alert().title('Remove Project').content('Are you sure you want to remove the project ?')
+		.ariaLabel('Removing project alert dialog').ok('Yes').targetEvent(ev));
+		// .then(function() {
+			// $location.path('/class/' + data.id + '/' + data.courseName); // Will display all the projects in this course
+		// }); // Pop-up alert
+	};
+
+	
 
 	$scope.getProjectInfo = function()
 	{
@@ -28,7 +37,15 @@ angular.module('SeHub')
 			$scope.project = data;
 			console.log($scope.project);
 			$scope.init_line_lables();
-			$scope.loadingData = false;
+			// if($scope.user === $scope.project.info.master_id)
+			// {
+			// 	$scope.isMasterOrLecturer = true;
+			// }
+			// if($scope.project && $scope.project.length > 0) // TODO - TEST?
+			// {
+				$scope.loadingData = false;
+			// }
+			
 			console.log($scope.project);
 		}).error(function(err)
 		{
