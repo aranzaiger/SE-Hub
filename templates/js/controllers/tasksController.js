1dafd10b7277c7a04f9e4212d9bebbd9aa96243a
Sagi Dayan <sagidayan@gmail.com>
2015-08-02 21:46:42 +0300

    New Task Layout

--- templates/js/controllers/tasksController.js
+++ templates/js/controllers/tasksController.js
@@ -1,32 +1,30 @@
 angular.module('SeHub')
-.controller('tasksController', ['$scope', '$cookies', '$cookieStore', '$window', '$location', '$mdToast', '$mdDialog', 'apiService', '$rootScope', function ($scope, $cookies, $cookieStore, $window, $location, $mdToast, $mdDialog, apiService ,$rootScope)
-{
-	$scope.lecturerTasks = [];
-	$rootScope.seToken = $cookies['com.sehub.www'];
-  	var token = $rootScope.seToken;
+	.controller('tasksController', ['$scope', '$cookies', '$cookieStore', '$window', '$location', '$mdToast',
+		'$mdDialog', 'apiService', '$rootScope',
+		function($scope, $cookies, $cookieStore, $window, $location, $mdToast, $mdDialog, apiService, $rootScope) {
+			$scope.lecturerTasks = [];
+			$rootScope.seToken = $cookies['com.sehub.www'];
+			var token = $rootScope.seToken;
 
-	$scope.createTaskClicked = function()
-	{
-		$location.path("/tasks/new"); // Reference to 'newTask' page
-	}
+			$scope.createTaskClicked = function() {
+				$location.path("/tasks/new"); // Reference to 'newTask' page
+			}
 
-	$scope.displayTasks = function()
-	{
-	    apiService.getAllFutureTasks(token).success(function(data) // Get all Tasks // TODO change to closest TASK
-	    {
-	      $scope.lecturerTasks = data;
-	      console.log(data);
-	    }).error(function(err)
-	    {
-	      console.log(err.message);
-	    });
-	}
-	$scope.gotoTask = function(taskId)
-	{
-		$location.path('/tasks/fill/' + taskId);
-	}
+			$scope.displayTasks = function() {
+				apiService.getAllFutureTasks(token).success(function(data) // Get all Tasks // TODO change to closest TASK
+					{
+						$scope.lecturerTasks = data;
+						console.log(data);
+					}).error(function(err) {
+					console.log(err.message);
+				});
+			}
+			$scope.gotoTask = function(taskId) {
+				$location.path('/tasks/fill/' + taskId);
+			}
 
-	$scope.displayTasks(); // Calling tasks with task id
+			$scope.displayTasks(); // Calling tasks with task id
 
 
-}]);
+		}
+	]);
